bf94361d741647b01ae3277cce61e977c5ae71c4
hstha <shresthaheriz14@gmail.com>
2020-12-29 22:45:16 +0545

    adding signup feature but getting issue while saving new user

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,11 +1,34 @@
-exports.signup = (req, res, next) => {
-    res.json({
-        data: 'you hit the signup api'
-    });
+const User = require('../models/user.model');
+
+exports.signup = async (req, res, next) => {
+    const { name, email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email }).exec();
+        if (user) {
+            res.status(400).json({
+                error: [
+                    {
+                        msg: 'Email is already taken'
+                    }
+                ]
+            });
+        }
+
+        let newUser = new User({ name, email, password });
+        const result = await newUser.save();
+        console.log(result);
+    } catch (err) {
+        res.status(404).json({
+            error: err
+        })
+    }
+
+
 };
 
 exports.login = (req, res, next) => {
     res.json({
         data: 'you hit the login api'
     });
-}
+};
