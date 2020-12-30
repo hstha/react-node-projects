98badfb118e4f1ecedc039fed7d73a8898ac4281
hstha <shresthaheriz14@gmail.com>
2020-12-30 07:20:19 +0545

    proper formatting

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -33,35 +33,35 @@ const userSchema = new Mongoose.Schema({
 
 // virtual
 userSchema.virtual('password')
-.set(function(password) {
-    this._password = password;
-    this._salt = this.makeSalt();
-    this.hashed_password = this.encryptPassword(password);
-})
-.get(function() {
-    return this._password;
-});
+    .set(function (password) {
+        this._password = password;
+        this.salt = this.makeSalt();
+        this.hashed_password = this.encryptPassword(password);
+    })
+    .get(function () {
+        return this._password;
+    });
 
 //methods
 userSchema.methods = {
-    authenticate: function(planText) {
+    authenticate: function (planText) {
         return this.encryptPassword(planText) === this.hashed_password;
     },
-    encryptPassword: function(password) {
-        if(!password) {
+    encryptPassword: function (password) {
+        if (!password) {
             return '';
         }
 
         try {
             return crypto
-                    .createHmac('sha1', this.salt)
-                    .update(password)
-                    .digest('hex')
-        }catch(e) {
+                .createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex')
+        } catch (e) {
             return '';
         }
     },
-    makeSalt: function() {
+    makeSalt: function () {
         return Math.round(new Date().valueOf() * Math.random()) + '';
     }
 }
