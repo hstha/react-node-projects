cdb2833001c742dd44e002be661feaf985e35c2f
hstha <shresthaheriz14@gmail.com>
2020-12-31 21:50:52 +0545

    added login validators

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const { runValidation } = require("../validators/index.validator");
-const { userSignupValidator } = require("../validators/auth.validator");
+const { userSignupValidator, userLoginValidator } = require("../validators/auth.validator");
 const { signup, login, activateAccount } = require('../controllers/auth.controller');
 
 const router = express.Router();
 
-router.post('/login', login);
 router.post('/activate-account', activateAccount);
+router.post('/login', userLoginValidator, runValidation, login);
 router.post('/signup', userSignupValidator, runValidation, signup);
 
 module.exports = router;
