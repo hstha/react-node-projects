03d5d48ec4a41f04f70a28b0fb7233eb5b7ceda6
hstha <shresthaheriz14@gmail.com>
2020-12-31 21:47:09 +0545

    added authentication process and console.logs

--- backend/src/models/User/User.js
+++ backend/src/models/User/User.js
@@ -1,15 +1,17 @@
+const { ERROR_MESSAGE } = require("../../app.constant");
 const UserModel = require("./user.model");
 
 class User {
 	static isUserPresent(email) {
+		console.log('[method: isUserPresent]');
 		return UserModel.findOne({ email })
 			.exec()
 			.then((result) => {
 				if (result) {
-					return true;
+					return result;
 				}
 
-				return false;
+				return null;
 			})
 			.catch((err) => {
 				return err;
@@ -17,6 +19,7 @@ class User {
 	}
 
 	static saveUser({ name, email, password }) {
+		console.log('[method: saveUser]');
 		const newUser = new UserModel({ name, email, password });
 		return newUser.save()
 			.then((result) => {
@@ -32,6 +35,39 @@ class User {
 			}).catch((err) => {
 				return err;
 			});
+	}
+
+	static getAuthenticatedUser({ email, password }) {
+		console.log('[method: getAuthenticatedUser]');
+		return new Promise((resolve, reject) => {
+			User.isUserPresent(email)
+				.then((result) => {
+					if (!result) {
+						reject({
+							message: ERROR_MESSAGE.NOT_REGISTERED_USER,
+							data: []
+						});
+					}
+
+					const isAuthenticated = result.authenticate(password);
+					if (!isAuthenticated) {
+						reject({
+							message: ERROR_MESSAGE.AUTHENTICATION_ERROR,
+							data: []
+						});
+					}
+
+					const { _id, name, email, role } = result;
+
+					resolve({ _id, name, email, role });
+				})
+				.catch(err => {
+					reject({
+						message: ERROR_MESSAGE.PROCESS_FAILED,
+						data: []
+					})
+				})
+		})
 	}
 }
 
