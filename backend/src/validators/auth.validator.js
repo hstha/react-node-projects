89fec07c477e726265ed01e56541fc7660789ea8
hstha <shresthaheriz14@gmail.com>
2020-12-31 21:47:39 +0545

    added login validator and console.log

--- backend/src/validators/auth.validator.js
+++ backend/src/validators/auth.validator.js
@@ -1,7 +1,14 @@
 const { check } = require('express-validator');
 
+console.log('[validator: auth]');
+
 exports.userSignupValidator = [
   check('name').not().isEmpty().withMessage('Name is required'),
+  check('email').isEmail().withMessage('Email must be valid'),
+  check('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+]
+
+exports.userLoginValidator = [
   check('email').isEmail().withMessage('Email must be valid'),
   check('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
 ]
