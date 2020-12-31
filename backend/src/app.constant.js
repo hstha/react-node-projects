75c45563ea67d616a9d69716d0e7db217e7dcc7f
hstha <shresthaheriz14@gmail.com>
2020-12-31 21:46:17 +0545

    updated app constant

--- backend/src/app.constant.js
+++ backend/src/app.constant.js
@@ -11,3 +11,17 @@ exports.RESET_PASSWORD_KEY = processEnv.JWT_RESET_PASSWORD;
 exports.EMAIL_TO = processEnv.EMAIL_TO;
 exports.EMAIL_FROM = processEnv.EMAIL_FROM;
 exports.EMAIL_VALIDATION_TIME = '24h'
+
+exports.ERROR_MESSAGE = {
+  FORBIDDEN_ACCESS: 'Forbidden Access',
+  NOT_REGISTERED_USER: 'User is not registered',
+  REGISTER_TOKEN_EXPIRED: 'Token has already expired, please sign up again',
+  PROCESS_FAILED: 'Failed to process the request, please try again',
+  EMAIL_TAKEN: 'Email is already taken',
+  AUTHENTICATION_ERROR: `Email and password doesn't match`
+}
+
+exports.SUCCESS_MESSAGE = {
+  ACCOUNT_CREATED: 'Account registered successfully',
+  SIGN_IN_SUCCESS: 'Logged in successfully'
+}
