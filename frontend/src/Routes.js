441551b6d6582909759dc1603a5a1ac094b79512
hstha <shresthaheriz14@gmail.com>
2021-01-02 08:42:46 +0545

    updating routes

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import App from './App';
+import Login from './components/auth/login/Login';
+import Signup from './components/auth/signup/Signup';
+import ActivateAccount from './components/auth/Activate';
 
 const Routes = ({ children }) => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path='/' component={App} />
+        <Route path='/' exact component={App} />
+        <Route path='/signup' component={Signup} />
+        <Route path='/login' component={Login} />
+        <Route path='/auth/activate/:token' component={ActivateAccount} />
       </Switch>
     </BrowserRouter>
   )
