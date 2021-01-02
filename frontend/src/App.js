5a84e518c88413f7f23bcb3ed328e45e5f5b7a04
hstha <shresthaheriz14@gmail.com>
2021-01-02 08:43:24 +0545

    updating app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Layout from './core/Layout';
+import Layout from './core/Layout.hoc';
 
 const App = () => {
   return (
     <Layout>
-      <h1>Hello React</h1>
+      <h1>Home Page</h1>
     </Layout>
   )
 }
