0bb93f3cc9e630c7e1f4d06d1b2ed7cd32e352dd
hstha <shresthaheriz14@gmail.com>
2021-01-02 17:21:14 +0545

    rendering routes conditionally

--- frontend/src/container/Navigation.js
+++ frontend/src/container/Navigation.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import Authentication from '../utils/Authentication.helper';
 
-const Navigation = ({ match }) => {
+const Navigation = ({ match, history }) => {
   const isActive = path => {
     if (match.path === path) {
       return true;
@@ -9,18 +10,50 @@ const Navigation = ({ match }) => {
 
     return false;
   }
+
+  const user = Authentication.getUser();
+
+  const logout = () => {
+    Authentication.remove();
+    history.push('/');
+  }
+
   return (
-    <ul className='nav nav-tabs bg-primary'>
-      <li className='nav-item'>
-        <Link className={`nav-link ${isActive('/') ? 'active' : 'text-light'}`} to='/'>Home</Link>
-      </li>
-      <li className='nav-item'>
-        <Link className={`nav-link ${isActive('/signup') ? 'active' : 'text-light'}`} to='/signup'>Signup</Link>
-      </li>
-      <li className='nav-item'>
-        <Link className={`nav-link ${isActive('/login') ? 'active' : 'text-light'}`} to='/login'>Login</Link>
-      </li>
-    </ul>
+    <nav className='bg-primary'>
+      <ul className='nav nav-tabs container'>
+        <li className='nav-item'>
+          <Link className={`nav-link ${isActive('/') ? 'active' : 'text-light'}`} to='/'>Home</Link>
+        </li>
+        {
+          !user
+            ?
+            <Fragment>
+              <li className='nav-item'>
+                <Link className={`nav-link ${isActive('/signup') ? 'active' : 'text-light'}`} to='/signup'>Signup</Link>
+              </li>
+              <li className='nav-item'>
+                <Link className={`nav-link ${isActive('/login') ? 'active' : 'text-light'}`} to='/login'>Login</Link>
+              </li>
+            </Fragment>
+            :
+            <Fragment>
+              <li className='nav-item'>
+                <Link className={`nav-link ${isActive('/profile') ? 'active' : 'text-light'}`} to='/profile'>Profile</Link>
+              </li>
+              {
+                user.role === 'admin' && (
+                  <li className='nav-item'>
+                    <Link className={`nav-link ${isActive('/admin') ? 'active' : 'text-light'}`} to='/admin'>Admin</Link>
+                  </li>
+                )
+              }
+              <li className='nav-item float-right'>
+                <span className='nav-link' onClick={logout}>Log Out</span>
+              </li>
+            </Fragment>
+        }
+      </ul>
+    </nav>
   )
 }
 
