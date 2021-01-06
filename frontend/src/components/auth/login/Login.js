15a4a1092681926085670b04f846c002fb059db7
Heriz Shrestha <heriz.shrestha@compliancequest.com>
2021-01-06 16:10:10 +0545

    fixed indentation and unfocus of input when typing

--- frontend/src/components/auth/login/Login.js
+++ frontend/src/components/auth/login/Login.js
@@ -1,4 +1,4 @@
-import { connect } from "react-redux";
+import { connect } from 'react-redux';
 import { toast } from 'react-toastify';
 import React, { useState } from 'react';
 import formData from './loginForm.data.json';
@@ -6,94 +6,101 @@ import Layout from '../../../core/Layout.hoc';
 import ServiceAPI from '../../../utils/fetch.api';
 import { BACKEND_API } from '../../../app.constant';
 import Authentication from '../../../utils/Authentication.helper';
-import { setUser } from "../../../store/actions/userActions";
+import { setUser } from '../../../store/actions/userActions';
 
 const Login = ({ history, user, setUser }) => {
-  const [value, setValue] = useState({
-    email: '',
-    password: ''
-  });
+	const [value, setValue] = useState({
+		email: '',
+		password: '',
+	});
 
-  const { isLoggedIn, role } = user;
+	const { isLoggedIn, role } = user;
 
-  const onChangeHandler = (e) => {
-    const stateKey = e.target.name;
-    const stateValue = e.target.value;
-    setValue({ ...value, [stateKey]: stateValue });
-  }
+	const onChangeHandler = (e) => {
+		const stateKey = e.target.name;
+		const stateValue = e.target.value;
+		setValue({ ...value, [stateKey]: stateValue });
+	};
 
-  const onSubmitHandler = (e) => {
-    e.preventDefault();
+	const onSubmitHandler = (e) => {
+		e.preventDefault();
 
-    ServiceAPI.post(`${BACKEND_API}/login`, value)
-      .then(result => {
-        if (!result.success) {
-          throw result;
-        }
-        const { token, user } = result.data;
-        Authentication.set({ token, user });
-        toast.success(result.message);
-        setValue({ email: '', password: '' });
-        setUser({ name: user.name, email: user.email, role: user.role, isLoggedIn: true });
-      })
-      .catch(err => {
-        console.log(err);
-        toast.error(err.message);
-      })
-  }
+		ServiceAPI.post(`${BACKEND_API}/login`, value)
+			.then((result) => {
+				if (!result.success) {
+					throw result;
+				}
+				const { token, user } = result.data;
+				Authentication.set({ token, user });
+				toast.success(result.message);
+				setValue({ email: '', password: '' });
+				setUser({
+					name: user.name,
+					email: user.email,
+					role: user.role,
+					isLoggedIn: true,
+				});
+			})
+			.catch((err) => {
+				console.log(err);
+				toast.error(err.message);
+			});
+	};
 
-  const LogInForm = () => (
-    <form onSubmit={onSubmitHandler}>
-      {
-        formData.map(data => {
-          const { id, label, name, type } = data;
-          return (
-            <div key={id} className="form-group">
-              <label htmlFor={data.name} className='text-muted'>{label}</label>
-              <input
-                type={type}
-                name={name}
-                className='form-control'
-                required={true}
-                onChange={onChangeHandler}
-                value={value[name]}
-              />
-            </div>
-          )
-        })
-      }
-      <div>
-        <button className='btn btn-primary' type='submit'>Sign up</button>
-      </div>
-    </form>
-  );
+	const logInForm = (
+		<form onSubmit={onSubmitHandler}>
+			{formData.map((data) => {
+				const { id, label, name, type } = data;
+				return (
+					<div key={id} className="form-group">
+						<label htmlFor={data.name} className="text-muted">
+							{label}
+						</label>
+						<input
+							type={type}
+							name={name}
+							className="form-control"
+							required={true}
+							onChange={onChangeHandler}
+							value={value[name]}
+						/>
+					</div>
+				);
+			})}
+			<div>
+				<button className="btn btn-primary" type="submit">
+					Sign up
+				</button>
+			</div>
+		</form>
+	);
 
-  if (isLoggedIn && role === 'admin') {
-    history.push('/admin');
-  } else if (isLoggedIn) {
-    history.push('/profile');
-  }
+	if (isLoggedIn && role === 'admin') {
+		history.push('/admin');
+	} else if (isLoggedIn) {
+		history.push('/profile');
+	}
 
-  return (
-    <Layout>
-      <div className='col-md-6 offset-md-3'>
-        <h1 className='p-5 text-center'>Login</h1>
-        <LogInForm />
-      </div>
-    </Layout>
-  );
-}
+	return (
+		<Layout>
+			<div className="col-md-6 offset-md-3">
+				<h1 className="p-5 text-center">Login</h1>
+				{logInForm}
+			</div>
+		</Layout>
+	);
+};
 
-const mapStateToProps = state => {
-  return ({
-    user: state.user
-  });
-}
+const mapStateToProps = (state) => {
+	return {
+		user: state.user,
+	};
+};
 
-const mapActionToProps = dispatch => {
-  return {
-    setUser: (user) => dispatch(setUser(user))
-  };
-}
+const mapActionToProps = (dispatch) => {
+	return {
+		setUser: (user) => dispatch(setUser(user)),
+	};
+};
 
 export default connect(mapStateToProps, mapActionToProps)(Login);
--- frontend/src/components/auth/signup/Signup.js
+++ frontend/src/components/auth/signup/Signup.js
@@ -1,88 +1,87 @@
-import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
 import { toast } from 'react-toastify';
-
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
+import Layout from '../../../core/Layout.hoc';
+import formData from './signupForm.data.json';
+import ServiceAPI from '../../../utils/fetch.api';
 import { BACKEND_API } from '../../../app.constant';
 
-import Layout from '../../../core/Layout.hoc';
-import Authentication from '../../../utils/Authentication.helper';
-import ServiceAPI from '../../../utils/fetch.api';
-import formData from './signupForm.data.json';
-
 const Signup = ({ user }) => {
-  const [value, setValue] = useState({
-    name: '',
-    email: '',
-    password: ''
-  });
+	const [value, setValue] = useState({
+		name: '',
+		email: '',
+		password: '',
+	});
 
-  const { isLoggedIn } = user;
+	const { isLoggedIn } = user;
 
-  const onChangeHandler = (e) => {
-    const stateKey = e.target.name;
-    const stateValue = e.target.value;
-    setValue({ ...value, [stateKey]: stateValue });
-  }
+	const onChangeHandler = (e) => {
+		const stateKey = e.target.name;
+		const stateValue = e.target.value;
+		setValue({ ...value, [stateKey]: stateValue });
+	};
 
-  const onSubmitHandler = (e) => {
-    e.preventDefault();
-    ServiceAPI.post(`${BACKEND_API}/signup`, value)
-      .then(result => {
-        if (!result.success) {
-          throw result;
-        }
-        setValue({ name: '', email: '', password: '' });
-        toast.success(result.message);
-      })
-      .catch(err => {
-        console.log(err);
-        toast.error(err.message);
-      })
-  }
+	const onSubmitHandler = (e) => {
+		e.preventDefault();
+		ServiceAPI.post(`${BACKEND_API}/signup`, value)
+			.then((result) => {
+				if (!result.success) {
+					throw result;
+				}
+				setValue({ name: '', email: '', password: '' });
+				toast.success(result.message);
+			})
+			.catch((err) => {
+				console.log(err);
+				toast.error(err.message);
+			});
+	};
 
-  const SignUpForm = () => (
-    <form onSubmit={onSubmitHandler}>
-      {
-        formData.map(data => {
-          const { id, label, name, type } = data;
-          return (
-            <div key={id} className="form-group">
-              <label htmlFor={data.name} className='text-muted'>{label}</label>
-              <input
-                type={type}
-                name={name}
-                className='form-control'
-                required={true}
-                onChange={onChangeHandler}
-                value={value[name]}
-              />
-            </div>
-          )
-        })
-      }
-      <div>
-        <button className='btn btn-primary' type='submit'>Sign up</button>
-      </div>
-    </form>
-  );
+	const signUpForm = (
+		<form onSubmit={onSubmitHandler}>
+			{formData.map((data) => {
+				const { id, label, name, type } = data;
+				return (
+					<div key={id} className="form-group">
+						<label htmlFor={data.name} className="text-muted">
+							{label}
+						</label>
+						<input
+							type={type}
+							name={name}
+							className="form-control"
+							required={true}
+							onChange={onChangeHandler}
+							value={value[name]}
+						/>
+					</div>
+				);
+			})}
+			<div>
+				<button className="btn btn-primary" type="submit">
+					Sign up
+				</button>
+			</div>
+		</form>
+	);
 
-  if (isLoggedIn) {
-    return <Redirect to='/login' />
-  }
+	if (isLoggedIn) {
+		return <Redirect to="/login" />;
+	}
 
-  return (
-    <Layout>
-      <div className='col-md-6 offset-md-3'>
-        <h1 className='p-5 text-center'>Signup</h1>
-        <SignUpForm />
-      </div>
-    </Layout>
-  );
-}
+	return (
+		<Layout>
+			<div className="col-md-6 offset-md-3">
+				<h1 className="p-5 text-center">Signup</h1>
+				{signUpForm}
+			</div>
+		</Layout>
+	);
+};
 
-const mapStateToProps = state => ({
-  user: state.user
+const mapStateToProps = (state) => ({
+	user: state.user,
 });
 
 export default connect(mapStateToProps)(Signup);
