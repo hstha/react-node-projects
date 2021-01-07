2e8da444282528ec03d95b3de35154e03b53f4a4
hstha <shresthaheriz14@gmail.com>
2021-01-07 08:47:18 +0545

    solving to/cc error

--- backend/src/utils/email.helpers.js
+++ backend/src/utils/email.helpers.js
@@ -1,5 +1,5 @@
 const sgMail = require('@sendgrid/mail');
-const { EMAIL_API, CLIENT_URL } = require('../app.constant');
+const { EMAIL_API, CLIENT_URL, EMAIL_FROM } = require('../app.constant');
 
 sgMail.setApiKey(EMAIL_API);
 
@@ -7,7 +7,7 @@ class Email {
   static getAccountActivationMail({ to, token }) {
     return {
       to: to,
-      from: 'shresthaheriz14@gmail.com', // Change to your verified sender
+      from: EMAIL_FROM, // Change to your verified sender
       subject: 'Account activation',
       text: 'Activation mail',
       html: `
@@ -20,12 +20,12 @@ class Email {
   static getForgetPasswordMail({ to, token }) {
     return {
       to: to,
-      from: 'shresthaheriz14@gmail.com', // Change to your verified sender
+      from: EMAIL_FROM, // Change to your verified sender
       subject: 'Reset Password',
       text: 'Reset Password',
       html: `
         <h1>Click here to reset password</h1>
-        <a href="${CLIENT_URL}/auth/activate/${token}">${token}</a>
+        <a href="${CLIENT_URL}/auth/password/reset/${token}">${token}</a>
       `,
     }
   }
