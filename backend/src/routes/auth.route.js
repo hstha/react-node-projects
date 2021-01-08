587f3e17d79e6b2ab62f89e736ccea03d48edc4b
hstha <shresthaheriz14@gmail.com>
2021-01-08 08:28:20 +0545

    changed put to post

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post('/activate-account', activateAccount);
 router.post('/login', userLoginValidator, runValidation, login);
 router.post('/signup', userSignupValidator, runValidation, signup);
-router.put('/forget-password', userForgetPasswordValidator, runValidation, forgotPassword);
+router.post('/forget-password', userForgetPasswordValidator, runValidation, forgotPassword);
 router.put('/reset-password', userResetPasswordValidator, runValidation, resetPassword);
 
 module.exports = router;
